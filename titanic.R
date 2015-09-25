7f8d69be429cde58d5c6c3d43a1bc499779536f4
vbeakovic <valter.beakovic@gmail.com>
2015-09-25 18:33:34 +0200

    titanic.R added

---
+++ titanic.R
@@ -0,0 +1,4 @@
+# Read csv function
+readData <- function(path.name, file.name, column.types, missing.types) {
+  read.csv(url(paste(path.name, file.name, sep = "")), colClasses = column.types, na.strings = missing.types)
+}
