32a69ecb632cd2e9d8ab74e7fc636ffe2f56e88e
vbeakovic <valter.beakovic@gmail.com>
2015-09-26 16:16:39 +0200

    histograms and barplots with ggplot2

--- titanic.R
+++ titanic.R
@@ -37,3 +37,23 @@ missmap(df.train, main = "Titanic Training Data - Missing Map", col = c("yellow"
 age.barplot <- ggplot(df.train, aes(x = Survived)) + geom_bar(stat = "bin", fill = "black", colour = "black" ) + ggtitle("Survived (passenger fate)") + scale_y_continuous("") + scale_x_discrete("", labels = c("Perished", "Survived"))
 age.barplot
 
+pclass.barplot <- ggplot(df.train, aes(x = Pclass)) + geom_bar(stat = "bin", fill = "firebrick", colour = "black" ) + ggtitle("Pclass (passenger traveling class)") + scale_y_continuous("") + scale_x_discrete("", labels = c("first", "second", "third")) 
+pclass.barplot
+
+sex.barplot <- ggplot(df.train, aes(x = Sex)) + geom_bar(stat = "bin", fill = "darkviolet", colour = "black" ) + ggtitle("Sex (gender)") + scale_y_continuous("") + scale_x_discrete("", labels = c("female", "male")) 
+sex.barplot
+
+age.hist <- ggplot(df.train, aes(x = Age)) + geom_histogram(breaks = seq(0, 80, by = 10), fill = "brown", colour = "black") + labs(x = "", y = "Frequency") + labs(title = "Age")
+age.hist
+
+sibsp.barplot <- ggplot(df.train, aes(x = as.factor(SibSp))) + geom_bar(stat = "bin", fill = "darkblue", colour = "black" ) + labs(title = "SibSp (siblings + spouse aboard)") + labs(x = "", y = "")  
+sibsp.barplot
+
+parch.barplot <- ggplot(df.train, aes(x = as.factor(Parch))) + geom_bar(stat = "bin", fill = "gray50", colour = "black" ) + labs(title = "Parch (parents + kids aboard)") + labs(x = "", y = "")  
+parch.barplot
+
+fare.hist <- ggplot(df.train, aes(x = Fare)) + geom_histogram(breaks = seq(0, 500, by = 50), fill = "darkgreen", colour = "black") + labs(x = "", y = "Frequency") + labs(title = "Fare (fee paid for ticket[s])")
+fare.hist
+
+embarked.barplot <- ggplot(df.train[!is.na(df.train$Embarked), ], aes(x = Embarked)) + geom_bar(stat = "bin", fill = "sienna", colour = "black" ) + ggtitle("Embarked (port of embarkation)") + scale_y_continuous("") + scale_x_discrete("", labels = c("Cherbourg", "Queenstown", "Southampton")) 
+embarked.barplot
